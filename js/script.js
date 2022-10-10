a176310ef0796a099602e418006dd2257e67b503
MatthiasCominelli <matticomi12@gmail.com>
2022-10-10 16:01:04 +0200

    Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 
-
 //creiamo un loop che considera tutti i numerdi da 1 a 100
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
-}    
+    // console.log(i);
+//aggiungiamo i 3 if/else nel caso siano multiplo di 3, 5 o entrambi
+
+if ((i % 3 == 0) && (i % 5 == 0)) {
+    console.log("FizzBuzz")
+
+} else if (i % 3 == 0) {
+    console.log("Fizz");
+
+} else if (i % 5 == 0) {
+    console.log("Buzz");
+
+} else {
+    console.log(i);
+}}
+
   
 
   
